10556a35d25209b91bd838c26013e1d10f56fcd9
AlexanderLorenzo <alexlorenzodev@gmail.com>
2022-06-08 12:47:12 -0400

    added an action button

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -6,7 +6,12 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
-
+    @IBOutlet weak var magicBall: UIImageView!
+    
+    @IBAction func tryMeButton(_ sender: Any) {
+        //pulls random image from ball array
+        magicBall.image = ballArray[Int.random(in: 0...4)]
+    }
+    
 }
 
